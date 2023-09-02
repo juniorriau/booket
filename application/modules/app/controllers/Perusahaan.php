add690136ce4b0a0cc51558e07f90d5e738e6b3d
juniorriau <juniorriau18@gmail.com>
2023-09-02 19:02:58 +0700

    update app : modify perusahaan, remove read view

--- application/modules/app/controllers/Perusahaan.php
+++ application/modules/app/controllers/Perusahaan.php
@@ -3,11 +3,15 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class Perusahaan extends CI_Controller
+class Perusahaan extends MY_Controller
 {
-    function __construct()
+
+    public function __construct()
     {
         parent::__construct();
+        parent::_auth($this->uri->uri_string());
+        $this->sess = $this->session->logged_in;
+        $this->rolelist = $this->sess['rolelist'][ucfirst($this->uri->segment(2))];
         $this->load->model('Perusahaan_model');
         $this->load->library('form_validation');
     }
@@ -16,13 +20,13 @@ class Perusahaan extends CI_Controller
     {
         $q = urldecode($this->input->get('q', TRUE));
         $start = intval($this->input->get('start'));
-        
+
         if ($q <> '') {
-            $config['base_url'] = base_url() . 'perusahaan/index.html?q=' . urlencode($q);
-            $config['first_url'] = base_url() . 'perusahaan/index.html?q=' . urlencode($q);
+            $config['base_url'] = base_url() . 'app/perusahaan/index?q=' . urlencode($q);
+            $config['first_url'] = base_url() . 'app/perusahaan/index?q=' . urlencode($q);
         } else {
-            $config['base_url'] = base_url() . 'perusahaan/index.html';
-            $config['first_url'] = base_url() . 'perusahaan/index.html';
+            $config['base_url'] = base_url() . 'app/perusahaan/index';
+            $config['first_url'] = base_url() . 'app/perusahaan/index';
         }
 
         $config['per_page'] = 10;
@@ -39,53 +43,40 @@ class Perusahaan extends CI_Controller
             'pagination' => $this->pagination->create_links(),
             'total_rows' => $config['total_rows'],
             'start' => $start,
+            'session' => $this->sess,
+            'rolelist' => $this->rolelist,
+            'template' => 'perusahaan/perusahaan_list',
+            'extracss' => '',
+            'extrajs' => '',
         );
-        $this->load->view('perusahaan/perusahaan_list', $data);
+        $this->load->view('base/content', $data);
     }
 
-    public function read($id) 
-    {
-        $row = $this->Perusahaan_model->get_by_id($id);
-        if ($row) {
-            $data = array(
-		'id' => $row->id,
-		'namaperusahaan' => $row->namaperusahaan,
-		'alamat' => $row->alamat,
-		'penanggungjawab' => $row->penanggungjawab,
-		'nomorpenanggungjawab' => $row->nomorpenanggungjawab,
-		'telepon' => $row->telepon,
-		'createdat' => $row->createdat,
-		'createdby' => $row->createdby,
-		'updatedat' => $row->updatedat,
-		'updatedby' => $row->updatedby,
-	    );
-            $this->load->view('perusahaan/perusahaan_read', $data);
-        } else {
-            $this->session->set_flashdata('message', 'Record Not Found');
-            redirect(site_url('perusahaan'));
-        }
-    }
-
-    public function create() 
+    public function create()
     {
         $data = array(
             'button' => 'Create',
-            'action' => site_url('perusahaan/create_action'),
-	    'id' => set_value('id'),
-	    'namaperusahaan' => set_value('namaperusahaan'),
-	    'alamat' => set_value('alamat'),
-	    'penanggungjawab' => set_value('penanggungjawab'),
-	    'nomorpenanggungjawab' => set_value('nomorpenanggungjawab'),
-	    'telepon' => set_value('telepon'),
-	    'createdat' => set_value('createdat'),
-	    'createdby' => set_value('createdby'),
-	    'updatedat' => set_value('updatedat'),
-	    'updatedby' => set_value('updatedby'),
-	);
-        $this->load->view('perusahaan/perusahaan_form', $data);
+            'action' => site_url('app/perusahaan/create_action'),
+            'id' => set_value('id'),
+            'namaperusahaan' => set_value('namaperusahaan'),
+            'alamat' => set_value('alamat'),
+            'penanggungjawab' => set_value('penanggungjawab'),
+            'nomorpenanggungjawab' => set_value('nomorpenanggungjawab'),
+            'telepon' => set_value('telepon'),
+            'createdat' => set_value('createdat'),
+            'createdby' => set_value('createdby'),
+            'updatedat' => set_value('updatedat'),
+            'updatedby' => set_value('updatedby'),
+            'session' => $this->sess,
+            'rolelist' => $this->rolelist,
+            'template' => 'perusahaan/perusahaan_form',
+            'extracss' => '',
+            'extrajs' => '',
+        );
+        $this->load->view('base/content', $data);
     }
-    
-    public function create_action() 
+
+    public function create_action()
     {
         $this->_rules();
 
@@ -93,50 +84,55 @@ class Perusahaan extends CI_Controller
             $this->create();
         } else {
             $data = array(
-		'namaperusahaan' => $this->input->post('namaperusahaan',TRUE),
-		'alamat' => $this->input->post('alamat',TRUE),
-		'penanggungjawab' => $this->input->post('penanggungjawab',TRUE),
-		'nomorpenanggungjawab' => $this->input->post('nomorpenanggungjawab',TRUE),
-		'telepon' => $this->input->post('telepon',TRUE),
-		'createdat' => $this->input->post('createdat',TRUE),
-		'createdby' => $this->input->post('createdby',TRUE),
-		'updatedat' => $this->input->post('updatedat',TRUE),
-		'updatedby' => $this->input->post('updatedby',TRUE),
-	    );
+                'namaperusahaan' => $this->input->post('namaperusahaan', TRUE),
+                'alamat' => $this->input->post('alamat', TRUE),
+                'penanggungjawab' => $this->input->post('penanggungjawab', TRUE),
+                'nomorpenanggungjawab' => $this->input->post('nomorpenanggungjawab', TRUE),
+                'telepon' => $this->input->post('telepon', TRUE),
+                'createdat' => date('Y-m-d H:i:s'),
+                'createdby' => $this->sess['id'],
+                'updatedat' => date('Y-m-d H:i:s'),
+                'updatedby' => $this->sess['id'],
+            );
 
             $this->Perusahaan_model->insert($data);
             $this->session->set_flashdata('message', 'Create Record Success');
-            redirect(site_url('perusahaan'));
+            redirect(site_url('app/perusahaan'));
         }
     }
-    
-    public function update($id) 
+
+    public function update($id)
     {
         $row = $this->Perusahaan_model->get_by_id($id);
 
         if ($row) {
             $data = array(
                 'button' => 'Update',
-                'action' => site_url('perusahaan/update_action'),
-		'id' => set_value('id', $row->id),
-		'namaperusahaan' => set_value('namaperusahaan', $row->namaperusahaan),
-		'alamat' => set_value('alamat', $row->alamat),
-		'penanggungjawab' => set_value('penanggungjawab', $row->penanggungjawab),
-		'nomorpenanggungjawab' => set_value('nomorpenanggungjawab', $row->nomorpenanggungjawab),
-		'telepon' => set_value('telepon', $row->telepon),
-		'createdat' => set_value('createdat', $row->createdat),
-		'createdby' => set_value('createdby', $row->createdby),
-		'updatedat' => set_value('updatedat', $row->updatedat),
-		'updatedby' => set_value('updatedby', $row->updatedby),
-	    );
-            $this->load->view('perusahaan/perusahaan_form', $data);
+                'action' => site_url('app/perusahaan/update_action'),
+                'id' => set_value('id', $row->id),
+                'namaperusahaan' => set_value('namaperusahaan', $row->namaperusahaan),
+                'alamat' => set_value('alamat', $row->alamat),
+                'penanggungjawab' => set_value('penanggungjawab', $row->penanggungjawab),
+                'nomorpenanggungjawab' => set_value('nomorpenanggungjawab', $row->nomorpenanggungjawab),
+                'telepon' => set_value('telepon', $row->telepon),
+                'createdat' => set_value('createdat', $row->createdat),
+                'createdby' => set_value('createdby', $row->createdby),
+                'updatedat' => set_value('updatedat', $row->updatedat),
+                'updatedby' => set_value('updatedby', $row->updatedby),
+                'session' => $this->sess,
+                'rolelist' => $this->rolelist,
+                'template' => 'perusahaan/perusahaan_form',
+                'extracss' => '',
+                'extrajs' => '',
+            );
+            $this->load->view('base/content', $data);
         } else {
             $this->session->set_flashdata('message', 'Record Not Found');
-            redirect(site_url('perusahaan'));
+            redirect(site_url('app/perusahaan'));
         }
     }
-    
-    public function update_action() 
+
+    public function update_action()
     {
         $this->_rules();
 
@@ -144,51 +140,45 @@ class Perusahaan extends CI_Controller
             $this->update($this->input->post('id', TRUE));
         } else {
             $data = array(
-		'namaperusahaan' => $this->input->post('namaperusahaan',TRUE),
-		'alamat' => $this->input->post('alamat',TRUE),
-		'penanggungjawab' => $this->input->post('penanggungjawab',TRUE),
-		'nomorpenanggungjawab' => $this->input->post('nomorpenanggungjawab',TRUE),
-		'telepon' => $this->input->post('telepon',TRUE),
-		'createdat' => $this->input->post('createdat',TRUE),
-		'createdby' => $this->input->post('createdby',TRUE),
-		'updatedat' => $this->input->post('updatedat',TRUE),
-		'updatedby' => $this->input->post('updatedby',TRUE),
-	    );
+                'namaperusahaan' => $this->input->post('namaperusahaan', TRUE),
+                'alamat' => $this->input->post('alamat', TRUE),
+                'penanggungjawab' => $this->input->post('penanggungjawab', TRUE),
+                'nomorpenanggungjawab' => $this->input->post('nomorpenanggungjawab', TRUE),
+                'telepon' => $this->input->post('telepon', TRUE),
+                'updatedat' => date('Y-m-d H:i:s'),
+                'updatedby' => $this->sess['id'],
+            );
 
             $this->Perusahaan_model->update($this->input->post('id', TRUE), $data);
             $this->session->set_flashdata('message', 'Update Record Success');
-            redirect(site_url('perusahaan'));
+            redirect(site_url('app/perusahaan'));
         }
     }
-    
-    public function delete($id) 
+
+    public function delete($id)
     {
         $row = $this->Perusahaan_model->get_by_id($id);
 
         if ($row) {
             $this->Perusahaan_model->delete($id);
             $this->session->set_flashdata('message', 'Delete Record Success');
-            redirect(site_url('perusahaan'));
+            redirect(site_url('app/perusahaan'));
         } else {
             $this->session->set_flashdata('message', 'Record Not Found');
-            redirect(site_url('perusahaan'));
+            redirect(site_url('app/perusahaan'));
         }
     }
 
-    public function _rules() 
+    public function _rules()
     {
-	$this->form_validation->set_rules('namaperusahaan', 'namaperusahaan', 'trim|required');
-	$this->form_validation->set_rules('alamat', 'alamat', 'trim|required');
-	$this->form_validation->set_rules('penanggungjawab', 'penanggungjawab', 'trim|required');
-	$this->form_validation->set_rules('nomorpenanggungjawab', 'nomorpenanggungjawab', 'trim|required');
-	$this->form_validation->set_rules('telepon', 'telepon', 'trim|required');
-	$this->form_validation->set_rules('createdat', 'createdat', 'trim|required');
-	$this->form_validation->set_rules('createdby', 'createdby', 'trim|required');
-	$this->form_validation->set_rules('updatedat', 'updatedat', 'trim|required');
-	$this->form_validation->set_rules('updatedby', 'updatedby', 'trim|required');
+        $this->form_validation->set_rules('namaperusahaan', 'namaperusahaan', 'trim|required|xss_clean');
+        $this->form_validation->set_rules('alamat', 'alamat', 'trim|required|xss_clean');
+        $this->form_validation->set_rules('penanggungjawab', 'penanggungjawab', 'trim|required|xss_clean');
+        $this->form_validation->set_rules('nomorpenanggungjawab', 'nomorpenanggungjawab', 'trim|required|xss_clean');
+        $this->form_validation->set_rules('telepon', 'telepon', 'trim|required|numeric|xss_clean');
 
-	$this->form_validation->set_rules('id', 'id', 'trim');
-	$this->form_validation->set_error_delimiters('<span class="text-danger">', '</span>');
+        $this->form_validation->set_rules('id', 'id', 'trim|xss_clean');
+        $this->form_validation->set_error_delimiters('<span class="text-danger">', '</span>');
     }
 
 }
--- application/modules/app/views/perusahaan/perusahaan_form.php
+++ application/modules/app/views/perusahaan/perusahaan_form.php
@@ -1,56 +1,82 @@
-<!doctype html>
-<html>
-    <head>
-        <title>harviacode.com - codeigniter crud generator</title>
-        <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>"/>
-        <style>
-            body{
-                padding: 15px;
-            }
-        </style>
-    </head>
-    <body>
-        <h2 style="margin-top:0px">Perusahaan <?php echo $button ?></h2>
-        <form action="<?php echo $action; ?>" method="post">
-	    <div class="form-group">
-            <label for="varchar">Namaperusahaan <?php echo form_error('namaperusahaan') ?></label>
-            <input type="text" class="form-control" name="namaperusahaan" id="namaperusahaan" placeholder="Namaperusahaan" value="<?php echo $namaperusahaan; ?>" />
+<!-- page title -->
+<header id="page-header">
+    <h1>
+        <?php echo ucfirst($this->uri->segment(2)) ?>
+    </h1>
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="<?php echo base_url($this->uri->segment(1)) ?>">Beranda</a></li>
+        <li class="breadcrumb-item "><a
+                href="<?php echo base_url($this->uri->segment(1) . "/" . $this->uri->segment(2)) ?>"><?php echo ucfirst($this->uri->segment(2)) ?></a></li>
+        <li class="breadcrumb-item active">
+            <?php echo ucfirst($this->uri->segment(3)) ?>
+        </li>
+    </ol>
+</header>
+<!-- /page title -->
+
+
+<div id="content" class="padding-20">
+    <form method="post" action="<?php echo $action ?>" autocomplete="off">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <?php echo ucfirst($this->uri->segment(2) . ' ' . $this->uri->segment(3)) ?>
+                    </div>
+                </div>
+            </div>
+            <div class="panel-body">
+                <div class="form-group">
+                    <label for="varchar">Nama Perusahaan
+                        <?php echo form_error('namaperusahaan') ?>
+                    </label>
+                    <input type="text" class="form-control" name="namaperusahaan" id="namaperusahaan"
+                        placeholder="Namaperusahaan" value="<?php echo $namaperusahaan; ?>" />
+                </div>
+                <div class="form-group">
+                    <label for="alamat">Alamat
+                        <?php echo form_error('alamat') ?>
+                    </label>
+                    <textarea class="form-control" rows="3" name="alamat" id="alamat"
+                        placeholder="Alamat"><?php echo $alamat; ?></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="varchar">Telepon
+                        <?php echo form_error('telepon') ?>
+                    </label>
+                    <input type="text" class="form-control" name="telepon" id="telepon" placeholder="Telepon"
+                        value="<?php echo $telepon; ?>" />
+                </div>
+                <div class="form-group">
+                    <label for="varchar">Penanggung Jawab
+                        <?php echo form_error('penanggungjawab') ?>
+                    </label>
+                    <input type="text" class="form-control" name="penanggungjawab" id="penanggungjawab"
+                        placeholder="Penanggungjawab" value="<?php echo $penanggungjawab; ?>" />
+                </div>
+                <div class="form-group">
+                    <label for="varchar">Nomor Penanggung Jawab
+                        <?php echo form_error('nomorpenanggungjawab') ?>
+                    </label>
+                    <input type="text" class="form-control" name="nomorpenanggungjawab" id="nomorpenanggungjawab"
+                        placeholder="Nomorpenanggungjawab" value="<?php echo $nomorpenanggungjawab; ?>" />
+                </div>
+            </div>
+            <div class="panel-footer">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div>
+                            <input type="hidden" name="id" value="<?php echo $id; ?>" />
+                            <input type="hidden" name="<?= $this->security->get_csrf_token_name() ?>"
+                                value="<?= $this->security->get_csrf_hash() ?>">
+                            <button type="submit" class="btn btn-primary">
+                                <?php echo $button ?>
+                            </button>
+                            <a href="<?php echo site_url('app/perusahaan') ?>" class="btn btn-danger">Batal</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-	    <div class="form-group">
-            <label for="alamat">Alamat <?php echo form_error('alamat') ?></label>
-            <textarea class="form-control" rows="3" name="alamat" id="alamat" placeholder="Alamat"><?php echo $alamat; ?></textarea>
-        </div>
-	    <div class="form-group">
-            <label for="varchar">Penanggungjawab <?php echo form_error('penanggungjawab') ?></label>
-            <input type="text" class="form-control" name="penanggungjawab" id="penanggungjawab" placeholder="Penanggungjawab" value="<?php echo $penanggungjawab; ?>" />
-        </div>
-	    <div class="form-group">
-            <label for="varchar">Nomorpenanggungjawab <?php echo form_error('nomorpenanggungjawab') ?></label>
-            <input type="text" class="form-control" name="nomorpenanggungjawab" id="nomorpenanggungjawab" placeholder="Nomorpenanggungjawab" value="<?php echo $nomorpenanggungjawab; ?>" />
-        </div>
-	    <div class="form-group">
-            <label for="varchar">Telepon <?php echo form_error('telepon') ?></label>
-            <input type="text" class="form-control" name="telepon" id="telepon" placeholder="Telepon" value="<?php echo $telepon; ?>" />
-        </div>
-	    <div class="form-group">
-            <label for="datetime">Createdat <?php echo form_error('createdat') ?></label>
-            <input type="text" class="form-control" name="createdat" id="createdat" placeholder="Createdat" value="<?php echo $createdat; ?>" />
-        </div>
-	    <div class="form-group">
-            <label for="int">Createdby <?php echo form_error('createdby') ?></label>
-            <input type="text" class="form-control" name="createdby" id="createdby" placeholder="Createdby" value="<?php echo $createdby; ?>" />
-        </div>
-	    <div class="form-group">
-            <label for="datetime">Updatedat <?php echo form_error('updatedat') ?></label>
-            <input type="text" class="form-control" name="updatedat" id="updatedat" placeholder="Updatedat" value="<?php echo $updatedat; ?>" />
-        </div>
-	    <div class="form-group">
-            <label for="int">Updatedby <?php echo form_error('updatedby') ?></label>
-            <input type="text" class="form-control" name="updatedby" id="updatedby" placeholder="Updatedby" value="<?php echo $updatedby; ?>" />
-        </div>
-	    <input type="hidden" name="id" value="<?php echo $id; ?>" /> 
-	    <button type="submit" class="btn btn-primary"><?php echo $button ?></button> 
-	    <a href="<?php echo site_url('perusahaan') ?>" class="btn btn-default">Cancel</a>
-	</form>
-    </body>
-</html>
+    </form>
+</div>
